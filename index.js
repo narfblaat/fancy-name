ccb6e25bce7947bdb63f45addf72d0d7fcbe5f3f
narfblaat <narfblaat@users.noreply.github.com>
2015-05-30 23:57:33 +0200

    zwei

--- index.js
+++ index.js
@@ -9,5 +9,13 @@ app.get('/search', function (req, res) {
   res.send('vegaCurry!');
 });
 
+app.get('/articles', function (req, res) {
+    res.json([
+        { name: "Dusch DAS", price: "EUR 0,50" },
+        { name: "Alpro Soya Pudding", price: "EUR 1,99" },
+        { name: "Alpro Soya Yoghurt", price: "EUR 1,69" },
+        { name: "Julian", price: "unbezahlbar" }
+    ]);
+});
 
 app.listen(3000);
---
+++ tables.sql
@@ -0,0 +1,7 @@
+CREATE TABLE articles (
+    ean     char(9) PRIMARY KEY,
+    title   varchar NOT NULL,
+    rrp     integer NOT NULL    
+);
+
+
